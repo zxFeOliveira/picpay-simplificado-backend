838919fdb21ebd7d269d0a919ba606a682919021
zxFeOliveira <fbmithrandir@outlook.com>
2024-12-06 14:00:57 -0300

    Wallet.java criada

---
+++ src/main/java/br/com/felipeborges/picpay_desafio_backend/wallet/Wallet.java
@@ -0,0 +1,15 @@
+package br.com.felipeborges.picpay_desafio_backend.wallet;
+
+import org.springframework.data.annotation.Id;
+
+import java.math.BigDecimal;
+
+public record Wallet (
+    @Id Long id,
+    String fullName,
+    Long cpf,
+    String email,
+    String password,
+    int type,
+    BigDecimal balance) {
+}
