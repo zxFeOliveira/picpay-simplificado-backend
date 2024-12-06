a3ccfbca82b63e9a371cf729173b0615a39df9b6
zxFeOliveira <fbmithrandir@outlook.com>
2024-12-06 13:56:08 -0300

    Transaction.java criada, habilitada auditoria banco

--- src/main/java/br/com/felipeborges/picpay_desafio_backend/PicpayDesafioBackendApplication.java
+++ src/main/java/br/com/felipeborges/picpay_desafio_backend/PicpayDesafioBackendApplication.java
@@ -2,7 +2,9 @@ package br.com.felipeborges.picpay_desafio_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jdbc.repository.config.EnableJdbcAuditing;
 
+@EnableJdbcAuditing
 @SpringBootApplication
 public class PicpayDesafioBackendApplication {
 
