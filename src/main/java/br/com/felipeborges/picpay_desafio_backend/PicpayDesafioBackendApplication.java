9e1a1d8a074f1e4c8f9dd8e9e20f7a45dde44c50
zxFeOliveira <fbmithrandir@outlook.com>
2024-12-06 08:50:55 -0300

    Projeto Criado

---
+++ src/main/java/br/com/felipeborges/picpay_desafio_backend/PicpayDesafioBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.felipeborges.picpay_desafio_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpayDesafioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpayDesafioBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/felipeborges/picpay_desafio_backend/PicpayDesafioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.felipeborges.picpay_desafio_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayDesafioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
